156f6f92e0603e00a325c8bc4531380147283575
Valentun2 <pocomaxawrkt@gmail.com>
2023-11-25 18:20:23 +0200

    update

--- src/4.ts
+++ src/4.ts
@@ -1,66 +1,50 @@
+class Key {
+  private signature: number = Math.random();
 
-
-
-class Key{
-  private signature : number = Math.random()
-  constructor(){
-
+  getSignature(): number {
+    return this.signature;
   }
-  getSignature():number{
-          return this.signature
-      }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
   }
-  
-  class Person {
-  private key:{};
-  
-    constructor(key:{}){
-      this.key = key
-    }
-  
-    getKey():{}{
-      return this.key
-    }
-  
-  }
-  
-  abstract class House{
-    private tenants : object[] = []
-    public door:boolean = false
-    private key:{}
-    constructor( private myKey:{}){
+}
 
-      this.key = myKey
+abstract class House {
+  private tenants: object[] = [];
+  public door: boolean = false;
+  constructor(protected key: Key) {}
 
-    }
-   
-  
-    public abstract openDoor(myKey:number):void;
+  public abstract openDoor(myKey: Key): void;
 
-    comeIn(person : {}) : void{
-      if(this.door){
-  this.tenants.push(person)
-      }
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
     }
   }
-  
-  
-  class MyHouse extends House{
-   openDoor(myKey: {}){
-   
-    if(myKey === key){
-      this.door = true
-      }
-  }
-  }
-  
-  const key = new Key();
-  
-  const house = new MyHouse(key);
-  const person = new Person(key);
-  house.openDoor(person.getKey());
+}
 
-  house.comeIn(person);
+class MyHouse extends House {
+  openDoor(myKey: Key): void {
+    if (myKey.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    } else this.door = false;
+  }
+}
 
+
+const key = new Key();
+  
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+
+
+house.comeIn(person);
 
 export {};
